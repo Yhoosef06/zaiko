4722871a3b75ff72bdc13304ce3b07b5742a5c33
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-03-14 11:19:17 +0800

    Update pdfReport.blade.php

--- resources/views/pages/pdfReport.blade.php
+++ resources/views/pages/pdfReport.blade.php
@@ -7,55 +7,80 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
+<style>
+    table {
+        margin-bottom: 20px
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid;
+        border-collapse: collapse;
+        text-align: center;
+    }
+
+    #intro_details {
+        margin-bottom: 10px;
+        font-size: 12pt
+    }
+
+    #purpose {
+        font-size: 12px;
+        margin-bottom: 20px;
+    }
+
+    .signees {
+        column-count: 2;
+    }
+</style>
+
 <body>
-    <div class="container text-center justify-content-center">
-        <span style="font-weight: 800; font-size:18pt">UNIVERSITY OF SAN JOSE-RECOLETOS <br>
-            Inventory Report
-        </span>
-        <p><strong>
-                @if ($purpose == null)
-                @else
-                    ({{ $purpose }})
-                @endif
-            </strong></p>
+    <div class="container" style="font-weight: 800; font-size:18pt; text-align:center">
+        UNIVERSITY OF SAN JOSE-RECOLETOS <br>
+        Inventory Report
+        <div class="container" id="purpose">
+            @if ($purpose == null)
+            @else
+                ({{ $purpose }})
+            @endif
+        </div>
     </div>
-    <div class="container" style="font-size:14pt">
+    <div class="container" id="intro_details">
         <strong>DATE PREPARED:</strong> {{ now()->format('m-d-Y') }} <br>
         <strong>DEPARTMENT / OFFICE:</strong> {{ $department }} <br>
         <strong>SPECIFIC LOCATION:</strong> {{ $location }}
     </div>
-    <div class="container text-left">
-        <table class="table table-bordered" style="width:auto;border: 1px solid black;">
-            <thead style="width:auto;border-bottom: 1px solid black;">
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
                 <tr>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">Serial #</th>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">Description of Item</th>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">QTY</th>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">UNIT #</th>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">Aquisition Date</th>
-                    <th scope="col" style="width:auto;border-right: 1px solid black;">Status</th>
+                    <th scope="col">Serial #</th>
+                    <th scope="col">Description of Item</th>
+                    <th scope="col">QTY</th>
+                    <th scope="col">UNIT #</th>
+                    <th scope="col">Aquisition Date</th>
+                    <th scope="col">Status</th>
                     <th scope="col">Inventory Tag</th>
                 </tr>
             </thead>
-            @foreach ($items as $item)
-                @if ($item->location == $location)
-                    <tbody>
-                            <tr>
-                                <th style="width:auto;border-right: 1px solid black;">{{ $item->serial_number }}</th>
-                                <td style="width:auto;border-right: 1px solid black;">{{ $item->item_description }}</td>
-                                <td style="width:auto;border-right: 1px solid black;">{{ $item->quantity }}</td>
-                                <td style="width:auto;border-right: 1px solid black;">{{ $item->unit_number }}</td>
-                                <td style="width:auto;border-right: 1px solid black;">{{ $item->aquisition_date }}</td>
-                                <td style="width:auto;border-right: 1px solid black;">{{ $item->status }}</td>
-                                <td>{{ $item->inventory_tag }}</td>
-                            </tr>
-                        {{-- <tr class=" bg-blue">
-                            <td>Hello</td>
-                        </tr> --}}
-                    </tbody>
-                @endif
-            @endforeach
+            <tbody>
+                @foreach ($items as $item)
+                    @if ($item->location == $location)
+                        <tr>
+                            <th>{{ $item->serial_number }}</th>
+                            <td>{{ $item->item_description }}</td>
+                            <td>{{ $item->quantity }}</td>
+                            <td>{{ $item->unit_number }}</td>
+                            <td>{{ $item->aquisition_date }}</td>
+                            <td>{{ $item->status }}</td>
+                            <td>{{ $item->inventory_tag }}</td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
         </table>
+        
     </div>
 
 </html>
