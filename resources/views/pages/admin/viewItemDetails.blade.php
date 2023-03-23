89cdfd901d98e02cb3cad2054fc34e89a44e45dd
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-03-23 14:13:09 +0800

    added qr code generator

--- resources/views/pages/admin/viewItemDetails.blade.php
+++ resources/views/pages/admin/viewItemDetails.blade.php
@@ -20,6 +20,21 @@
                     <strong>Status:</strong> {{ $item->status }}
                 </div>
             </div>
+
+            <div class="card text-center">
+                <div class="card-header">
+                    <h5>{{ $item->serial_number }} QR Code</h5>
+                </div>
+                <div class="card-body">
+                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('helloworld')) !!} ">
+                    {{-- <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-1.png',.3, true)->size(200)->generate('http://www.simplesoftware.io');) !!} "> --}}
+
+                    {{-- {{ QrCode::size(120)->generate($item->serial_number) }} --}}
+
+                </div>
+                <a href="">Download</a>
+            </div>
+
             <hr>
             <a href="{{ route('view_items') }}" class="btn btn-outline-dark">Back</a>
         </div>
