973eae6b6e6c5d927527b2c5332871e15a8a3857
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-03-23 15:36:03 +0800

    Update viewItemDetails.blade.php

--- resources/views/pages/admin/viewItemDetails.blade.php
+++ resources/views/pages/admin/viewItemDetails.blade.php
@@ -26,13 +26,13 @@
                     <h5>{{ $item->serial_number }} QR Code</h5>
                 </div>
                 <div class="card-body">
-                    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('helloworld')) !!} ">
+                    {{-- <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('helloworld')) !!} "> --}}
                     {{-- <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-1.png',.3, true)->size(200)->generate('http://www.simplesoftware.io');) !!} "> --}}
 
-                    {{-- {{ QrCode::size(120)->generate($item->serial_number) }} --}}
+                    {{ QrCode::size(120)->generate($item->serial_number) }}
 
                 </div>
-                <a href="">Download</a>
+                {{-- <a href="">Download</a> --}}
             </div>
 
             <hr>
