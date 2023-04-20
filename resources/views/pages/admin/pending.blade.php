82ad9108e1836e3d05e1e16bcf82b3793e44ff43
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-04-20 15:35:31 +0800

    removed profile image
    removed profile image not relevant because no profile picture in user data

--- resources/views/pages/admin/pending.blade.php
+++ resources/views/pages/admin/pending.blade.php
@@ -22,7 +22,7 @@
 
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Pending Items</h3>
+                <h3 class="card-title"><strong>Pending Items</strong></h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
