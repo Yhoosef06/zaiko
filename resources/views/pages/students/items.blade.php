c783458b30e318cb1bcb82b87862a8f5e7fb8c4f
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-05-24 05:02:11 +0800

    Update items.blade.php

--- resources/views/pages/students/items.blade.php
+++ resources/views/pages/students/items.blade.php
@@ -8,11 +8,6 @@
                 <div class="col-sm-6">
                     <h1 class="m-0">Borrowing</h1>
                 </div>
-                {{-- <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Items available to borrow</li>
-                    </ol>
-                </div> --}}
             </div>
         </div>
     </div>
