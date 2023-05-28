bbc5b80553b43973caa1cf2aeb6fbc0909c79972
callmefrancisl <51121980+callmefrancisl@users.noreply.github.com>
2023-05-29 00:46:17 +0800

    11

--- resources/views/pages/students/history.blade.php
+++ resources/views/pages/students/history.blade.php
@@ -47,7 +47,7 @@
                                     <div class="modal-dialog modal-xl" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header text-dark">
-                                            <h5 class="modal-title" id="itemModalLabel"><b>ORDER HISTORY</b></h5>
+                                            <h5 class="modal-title" id="itemModalLabel"><b>ORDER HISTORY - ORDER ID {{$order->id}}</b></h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
