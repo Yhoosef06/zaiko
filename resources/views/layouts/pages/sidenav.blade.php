fc1a1e4c4578d973a2a7d7c429d1051c0de27eeb
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-05-29 13:33:00 +0800

    Update sidenav.blade.php

--- resources/views/layouts/pages/sidenav.blade.php
+++ resources/views/layouts/pages/sidenav.blade.php
@@ -42,7 +42,7 @@
                     <li class="nav-item">
                         <a href="{{ route('student.items') }}" class="nav-link">
                             <i class="fas fa-circle nav-icon"></i>
-                            <p>Browse Items</p>
+                            <p>Browse Items <i class="fa fa-search"></i></p>
                         </a>
                     </li>
                     <li class="nav-item">
