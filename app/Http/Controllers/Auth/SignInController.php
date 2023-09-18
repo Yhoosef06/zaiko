ae8692e6886656494bd01d5c8f778dc1db2dc2b2
callmefrancisl <51121980+callmefrancisl@users.noreply.github.com>
2023-09-18 17:19:58 +0800

    signincontroller changes, added role in conditions

--- app/Http/Controllers/Auth/SignInController.php
+++ app/Http/Controllers/Auth/SignInController.php
@@ -24,14 +24,14 @@ class SignInController extends Controller
         if (auth()->attempt(['id_number' => $input['id_number'], 'password' => $input['password']])) {
             if (auth()->user()->account_type == 'admin') {
                 return redirect()->route('admin.dashboard');
-            } else if (auth()->user()->account_type == 'reads') {
-                $userId = auth()->user()->id_number;
-                $user = User::find($userId);
-                $user->update([
-                    'last_login_at' => now()
-                ]);
-                return redirect()->route('admin.dashboard');
-            } else if (auth()->user()->account_type == 'faculty') {
+            // } else if (auth()->user()->account_type == 'reads') {
+            //     $userId = auth()->user()->id_number;
+            //     $user = User::find($userId);
+            //     $user->update([
+            //         'last_login_at' => now()
+            //     ]);
+            //     return redirect()->route('admin.dashboard');
+            } else if (auth()->user()->account_type == 'faculty' && auth()->user()->role == 'manager') {
                 $userId = auth()->user()->id_number;
                 $user = User::find($userId);
                 if ($user) {
@@ -40,7 +40,20 @@ class SignInController extends Controller
                     ]);
                 }
                 return redirect()->route('admin.dashboard');
-            } else if (auth()->user()->account_type == 'student') {
+            } else if (auth()->user()->account_type == 'faculty' && auth()->user()->role == 'borrower') {
+                $userId = auth()->user()->id_number;
+                $user = User::find($userId);
+                if ($user) {
+                    $user->update([
+                        'last_login_at' => now()
+                    ]);
+                }
+                if (auth()->user()->account_status == 'approved') {
+                    return redirect()->route('student.dashboard');
+                } else {
+                    return redirect()->route('approval');
+                }
+            } else if (auth()->user()->account_type == 'student' && auth()->user()->role == 'borrower') {
                 $userId = auth()->user()->id_number;
                 $user = User::find($userId);
                 if ($user) {
