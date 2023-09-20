b6860fdd7d7665934ee14d207fa94c8a58738dec
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-09-20 23:27:26 +0800

    Update listOfItems.blade.php

--- resources/views/pages/admin/listOfItems.blade.php
+++ resources/views/pages/admin/listOfItems.blade.php
@@ -11,13 +11,10 @@
         </div><!-- /.container-fluid -->
     </section>
 
-
     <section class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-
-
                     <div class="card">
                         <div class="card-header">
                             @if (session('success'))
