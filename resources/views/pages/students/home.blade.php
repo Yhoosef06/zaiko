9b57fbc528f0deb9a739c56fdc6b497825111e71
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-09-27 19:09:38 +0800

    Update home.blade.php

--- resources/views/pages/students/home.blade.php
+++ resources/views/pages/students/home.blade.php
@@ -102,23 +102,50 @@
                                                 <ul class="products-list product-list-in-card pl-2 pr-2">
                                                     <li class="item">
                                                         <div class="container">
-                                                            <div class="text-center">
-                                                                <h3><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie velit tellus, in consequat ante posuere quis.</strong></h3> 
-                                                                <h5>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</h5>
-                                                                <h5>Integer eu fringilla arcu. Proin risus neque, consectetur eget metus id, tempor tempor lectus.</h5>
-                                                                <h5> Donec lacinia nunc id scelerisque venenatis. Morbi ornare mattis turpis et fringilla.</h5>
-                                                                <h4><b>Aenean tincidunt, orci pellentesque luctus varius, risus ligula blandit augue.</b></h4>
-                                                            </div>
+                                                        <div class="card">
+                                    <div class="card-header bg-warning">
+                                        <h3 class="card-title">
+                                            <strong>Notifications</strong>
+                                        </h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body p-0" style="max-height:100px; overflow-y: auto;">
+                                        <ul class="products-list product-list-in-card pl-2 pr-2">
+                                            <!-- {{-- <li class="item">
+                                            <div class="product-img">
+                                                <img src="dist/img/default-150x150.png" alt="Product Image"
+                                                    class="img-size-50">
+                                            </div>
+                                            <div class="product-info">
+                                                <a href="javascript:void(0)" class="product-title">Samsung TV
+                                                    <span class="badge badge-warning float-right">$1800</span></a>
+                                                <span class="product-description">
+                                                    Samsung 32" 1080p 60Hz LED Smart HDTV.
+                                                </span>
+                                            </div>
+                                        </li> --}} -->
+                                            <li class="item">
+                                                <div class="container">
+                                                    <div class="text-center">
+                                                        No Messeages.
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                            </div>
                                                         </div>
-                                                    </li>
-                                                    <li class="item">
-                                                        <div class="container">
+                                                  
+                                                  
+                                                        <div class="container mb-2">
                                                             <div class="text-center">
                                                                 <a href="{{ route('student.items') }}" class="btn btn-lg btn-warning">Start Borrowing</a>
                                                             </div>
                                                         </div>
-                                                    </li>
-                                                </ul>
+                                                    
+                                                
                                             </div>
                                             <!-- /.card-body -->
                                         </div>
