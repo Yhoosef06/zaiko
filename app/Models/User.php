5c4d673f07668d1f56171e48badf93b321c20852
callmefrancisl <51121980+callmefrancisl@users.noreply.github.com>
2023-10-12 13:31:24 +0800

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -106,4 +106,15 @@ class User extends Authenticatable
 
         return false;
     }
+    public function hasRole($permission)
+    {
+        // Loop through the user's roles
+        foreach ($this->roles as $role) {
+            if ($role->permissions->contains('name', $permission)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
