ed353fc350542b9354c347fed9391462660dbb21
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-11-27 01:02:13 +0800

    Update CsvController.php

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Imports\CsvImport;
 use App\Jobs\CsvImportJob;
+use App\Models\Term;
 use Illuminate\Http\Request;
 use App\Mail\TemporaryPasswordEmail;
 use App\Models\UserDepartment;
@@ -14,6 +15,10 @@ class CsvController extends Controller
 {
     public function store(Request $request)
     {
+        $term = Term::where('isCurrent', true)->count() === 0;
+        if ($term) {
+            return redirect()->back()->with('danger', 'Please setup a school term first.');
+        }
         try {
             $request->validate([
                 'csv_file' => 'required|file|mimes:csv,txt|max:2048',
