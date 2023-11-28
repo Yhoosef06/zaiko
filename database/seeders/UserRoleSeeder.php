7d41c21b70008f6602848d5160a0206de1ab6f17
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-11-28 14:03:15 +0800

    updated seeder

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -20,28 +20,8 @@ class UserRoleSeeder extends Seeder
                 'role_id' => 1
             ],
             [
-                'user_id_number' => 1111,
-                'role_id' => 3
-            ],
-            [
                 'user_id_number' => 2016013001,
                 'role_id' => 2
-            ],
-            [
-                'user_id_number' => 2014036392,
-                'role_id' => 2
-            ],
-            [
-                'user_id_number' => 2012321404,
-                'role_id' => 3
-            ],
-            [
-                'user_id_number' => 2014571321,
-                'role_id' => 3
-            ],
-            [
-                'user_id_number' => 2014175231,
-                'role_id' => 3
             ],
         ];
         DB::table('user_roles')->insert($userRole);
