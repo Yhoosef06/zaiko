21408002a4b0e9cd3e077ac8e23eb1f7c8cd44f7
Yhoosef06 <111369811+Yhoosef06@users.noreply.github.com>
2023-12-11 00:52:55 +0800

    updated signin page

--- resources/views/pages/signin.blade.php
+++ resources/views/pages/signin.blade.php
@@ -7,14 +7,13 @@
         }
     </style>
     <div class="container col-xl-10 col-xxl-8 px-4 py-5">
-        <div class="row align-items-center g-lg-5 py-5">
-            <div class="col-lg-7 text-center text-lg-start">
-                <h1 class="display-4 fw-bold lh-1 mb-3">Zaiko.</h1>
-                <p class="col-lg-10 fs-4">Manage, track and maintain your laboratory assets.</p>
-            </div>
-            <div class="col-md-10 mx-auto col-lg-5">
+        <div class="row align-items-center g-lg-5 py-5 my-5 custom-background">
+            <div class="col-md-10 mx-auto col-lg-5 shadow-lg">
                 <form class="p-4 p-md-5 border rounded-3 bg-light" method="POST" action="{{ route('signin') }}">
-
+                    @csrf
+                    <div class="form-floating mb-3">
+                        <h5 class="display-4 fw-bold lh-1 mb-3">Zaiko.</h5>
+                    </div>
                     @if (session('status'))
                         <div class="text-danger">
                             <p>{{ session('status') }}</p>
@@ -24,11 +23,9 @@
                             <p><strong>{{ session('message') }}</strong></p>
                         </div>
                     @endif
-
-                    @csrf
                     <div class="form-floating mb-3">
                         <input class="form-control @error('id_number') border-danger @enderror" name="id_number"
-                            id="id_number" placeholder="I.D. Number">
+                            id="id_number" placeholder="I.D. Number" value="{{ old('id_number') }}">
                         <label for="floatingInput">I.D. Number</label>
                         @error('id_number')
                             <div class="text-danger">
@@ -56,9 +53,18 @@
                     </div>
                     <hr>
                     <button class="w-100 btn btn-lg btn-success" type="submit">Sign In</button>
-    
                 </form>
+            </div>
+            <div class="col-lg-7 text-center text-lg-start">
             </div>
         </div>
     </div>
+
+    <style>
+        .custom-background {
+            background-image: url('{{ asset('images/usjr main.jpg') }}');
+            background-position: center;
+            background-size: cover;
+        }
+    </style>
 @endsection
